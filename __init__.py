47a337d90c6f7ce4a530d81be36474d9c4109f12
L. Barré <155089495+Gallienn@users.noreply.github.com>
2024-07-01 13:47:18 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,7 +3,11 @@ from flask import render_template
 from flask import json
 import sqlite3
 
-app = Flask(__name__)      
+app = Flask(__name__)     
+
+@app.route('/calcul_carre/<int:val_user>')
+def carre(val_user):
+    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
 
 @app.route("/contact/")
 def MaPremiereAPI():
